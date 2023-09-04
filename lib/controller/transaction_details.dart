54c1cd2b447b3f19f63a49d324442409608b1fe1
sufad M <sufadm77@gmail.com>
2023-09-04 13:49:29 +0530

    test added

--- lib/controller/transaction_details.dart
+++ lib/controller/transaction_details.dart
@@ -5,8 +5,7 @@ import 'package:marlo_project/services/login_service.dart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<List<Data>> fetchData() async {
-  final url = Uri.parse(
-      'https://asia-southeast1-marlo-bank-dev.cloudfunctions.net/api_dev/v2/airwallex/995b1e2e-c5ac-417b-afe5-1de5e92f4cf3/transfers');
+  final url = Uri.parse('');
   final shared = await SharedPreferences.getInstance();
   String? accessToken = shared.getString('access');
 
